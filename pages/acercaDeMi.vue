14a789234e074d88223884dc4c06e14b0e8fbdcd
Sebastian <poveda1999@gmail.com>
2021-08-11 09:39:48 -0500

    se agrego carton

--- pages/acercaDeMi.vue
+++ pages/acercaDeMi.vue
@@ -23,7 +23,7 @@
         components:{headerB,footerB,marco}, 
             data(){
             return{
-                certificaciones:["https://i.imgur.com/foUVgC8.jpg","https://i.imgur.com/Ure1lKA.jpg","https://i.imgur.com/Z80Zh97.jpg","https://i.imgur.com/mJKrwBD.jpg","https://i.imgur.com/0Kxfzpp.jpg","https://i.imgur.com/MJ1lwQL.jpg","https://i.imgur.com/Rf3mDfX.jpg","https://i.imgur.com/c1TAeDc.jpg","https://i.imgur.com/HFzTZX6.jpg","https://i.imgur.com/3ZYmstD.jpg","https://i.imgur.com/hDbf6tU.jpg","https://i.imgur.com/SIEafh3.jpg","https://i.imgur.com/boY4Hgn.jpg","https://i.imgur.com/B2Qa2wN.jpg","https://i.imgur.com/6tZauxL.jpg","https://i.imgur.com/uQC6aj6.jpg","https://i.imgur.com/s6ENlii.jpg","https://i.imgur.com/c8I5sx0.jpg","https://i.imgur.com/9fCynSO.jpg","https://i.imgur.com/2d4h3do.jpg","https://i.imgur.com/gnAWuS7.jpg","https://i.imgur.com/xwPG08y.jpg"]                
+                certificaciones:["https://i.imgur.com/foUVgC8.jpg","https://i.imgur.com/Ure1lKA.jpg","https://i.imgur.com/Z80Zh97.jpg","https://i.imgur.com/mJKrwBD.jpg","https://i.imgur.com/0Kxfzpp.jpg","https://i.imgur.com/MJ1lwQL.jpg","https://i.imgur.com/Rf3mDfX.jpg","https://i.imgur.com/c1TAeDc.jpg","https://i.imgur.com/HFzTZX6.jpg","https://i.imgur.com/3ZYmstD.jpg","https://i.imgur.com/hDbf6tU.jpg","https://i.imgur.com/SIEafh3.jpg","https://i.imgur.com/boY4Hgn.jpg","https://i.imgur.com/B2Qa2wN.jpg","https://i.imgur.com/6tZauxL.jpg","https://i.imgur.com/uQC6aj6.jpg","https://i.imgur.com/s6ENlii.jpg","https://i.imgur.com/c8I5sx0.jpg","https://i.imgur.com/9fCynSO.jpg","https://i.imgur.com/2d4h3do.jpg","https://i.imgur.com/gnAWuS7.jpg","https://i.imgur.com/xwPG08y.jpg","https://i.imgur.com/t5sqNkO.jpg"]                
             }    
         }
     }
